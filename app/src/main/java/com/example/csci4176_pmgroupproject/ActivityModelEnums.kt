3bb0c1aca2b72dba63e1a81bcad1c6f39fe8f206
Samuel Garcia <sam.garcia@dal.ca>
2024-03-14 20:35:24 -0300

    Models

---
+++ app/src/main/java/com/example/csci4176_pmgroupproject/ActivityModelEnums.kt
@@ -0,0 +1,11 @@
+package com.example.csci4176_pmgroupproject
+
+enum class ActivityModelEnums { CHECKED, TIMED, COUNTABLE }
+
+enum class ActivityModelRepeat { NEVER, DAILY, WEEKLY, BI_WEEKLY, TRI_WEEKLY, MONTHLY, CUSTOM }
+
+enum class ActivityModelDays { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
+
+enum class ActivityMood { DISAPPOINTED, NEUTRAL, ACCOMPLISHED }
+
+enum class ActivityEnergy { TIRED, NEUTRAL, ENERGIZED }
--- app/src/main/java/com/example/csci4176_pmgroupproject/Habit.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/Habit.kt
@@ -1,5 +1,5 @@
 package com.example.csci4176_pmgroupproject
 
 class Habit(val name: String) {
-    lateinit var Activities: ArrayList<Activity>
+    lateinit var Activities: ArrayList<TaskModel>
 }
