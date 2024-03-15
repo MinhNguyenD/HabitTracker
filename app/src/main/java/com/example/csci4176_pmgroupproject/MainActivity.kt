945704ded559825017114684beab79f9530456f6
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-03-14 23:20:00 -0300

    Added logic to switch activites when navigation items are clicked

--- app/src/main/java/com/example/csci4176_pmgroupproject/MainActivity.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.csci4176_pmgroupproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startActivity(Intent(this, loginActivity::class.java))//this is only for testing rn
+
+//        val navbarFragment = NavigationBar()
+//
+//        // Add navbar fragment to the activity
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.navbarFrame, navbarFragment)
+//            .commit()
     }
 }
