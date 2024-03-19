14b8c23d41d0b1e376c61d148bd430181c806bff
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-03-19 11:40:18 -0300

    Updated Activity class

--- app/src/main/java/com/example/csci4176_pmgroupproject/ActivityModelEnums.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/ActivityModelEnums.kt
@@ -2,9 +2,9 @@ package com.example.csci4176_pmgroupproject
 
 enum class ActivityModelEnums { CHECKED, TIMED, COUNTABLE }
 
-enum class ActivityModelRepeat { NEVER, DAILY, WEEKLY, BI_WEEKLY, TRI_WEEKLY, MONTHLY, CUSTOM }
+enum class ActivityModelFrequency { NEVER, DAILY, WEEKLY, BI_WEEKLY, TRI_WEEKLY, MONTHLY, CUSTOM }
 
-enum class ActivityModelDays { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
+enum class ActivityModelDayOfWeek { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
 
 enum class ActivityMood { DISAPPOINTED, NEUTRAL, ACCOMPLISHED }
 
--- app/src/main/java/com/example/csci4176_pmgroupproject/TaskModel.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/TaskModel.kt
@@ -4,10 +4,14 @@ import com.google.android.gms.tasks.Task
 import java.util.Date
 
 /*ActivityModel Redesigned*/
-open class TaskModel (var taskId : String, val userId : String, val habitId : String, val title: String, val type: ActivityModelEnums,
+open class TaskModel (var taskId : String, val userId : String, val habitId : String, var title: String, val type: ActivityModelEnums,
+                      var frequency : ActivityModelFrequency, var dayOfWeek: ActivityModelDayOfWeek,
                       var streak: Int, var mood: ActivityMood,
                       var energy: ActivityEnergy, var note: String){
-
+    constructor(userId : String,  habitId : String,  title: String,  type: ActivityModelEnums,
+                frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek,
+                 streak: Int,  mood: ActivityMood,
+                 energy: ActivityEnergy,  note: String) : this("", userId,habitId, title, type, frequency, dayOfWeek, streak,  mood, energy,  note)
     fun updateStreak(){ streak++; }
 
     fun breakStreak(){ streak = 0 }
@@ -21,18 +25,20 @@ open class TaskModel (var taskId : String, val userId : String, val habitId : St
     fun appendNote(note: String, tag: String){ this.note += "\n$tag $note"}
 }
 
-class CheckedTaskModel(taskId : String, userId: String, habitId : String, title: String)
-    : TaskModel(taskId, userId, habitId, title, ActivityModelEnums.CHECKED,
+class CheckedTaskModel(taskId : String, userId: String, habitId : String, title: String, frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek)
+    : TaskModel(taskId, userId, habitId, title, ActivityModelEnums.CHECKED, frequency,dayOfWeek,
     0, ActivityMood.NEUTRAL, ActivityEnergy.NEUTRAL, ""){
         // require by firebase
-        constructor() : this("","","","")
+        constructor() : this("","","","", ActivityModelFrequency.NEVER, ActivityModelDayOfWeek.MONDAY)
+        constructor(userId: String, habitId : String, title: String, frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek) : this("",userId,habitId, title, frequency,dayOfWeek)
 }
 
-class TimedTaskModel(taskId: String, userId: String, habitId : String, title: String, var startTime: Long)
-    : TaskModel(taskId, userId, habitId,title, ActivityModelEnums.TIMED,
+class TimedTaskModel(taskId: String, userId: String, habitId : String, title: String, frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek, var startTime: Long)
+    : TaskModel(taskId, userId, habitId,title, ActivityModelEnums.TIMED, frequency,dayOfWeek,
     0, ActivityMood.NEUTRAL, ActivityEnergy.NEUTRAL, ""){
         // require by firebase
-        constructor() : this("","","","",0)
+        constructor() : this("","","","", ActivityModelFrequency.NEVER, ActivityModelDayOfWeek.MONDAY,0)
+    constructor(userId: String, habitId : String, title: String, frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek, startTime: Long) : this("",userId,habitId, title, frequency, dayOfWeek, startTime)
     /*
      * This will return the total time the user spent on
      * this task/activity.
@@ -43,11 +49,12 @@ class TimedTaskModel(taskId: String, userId: String, habitId : String, title: St
     }
 }
 
-class CountableTaskModel(taskId: String, userId: String, habitId : String, title: String, var remaining:Int = 0)
-    : TaskModel(taskId, userId, habitId,title, ActivityModelEnums.COUNTABLE,
+class CountableTaskModel(taskId: String, userId: String, habitId : String, title: String, frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek, var remaining:Int = 0)
+    : TaskModel(taskId, userId, habitId,title, ActivityModelEnums.COUNTABLE, frequency, dayOfWeek,
     0, ActivityMood.NEUTRAL, ActivityEnergy.NEUTRAL, ""){
     // require by firebase
-    constructor() : this("","","","",0)
+    constructor() : this("","","","", ActivityModelFrequency.NEVER, ActivityModelDayOfWeek.MONDAY,0)
+    constructor(userId: String, habitId : String, title: String,frequency: ActivityModelFrequency, dayOfWeek: ActivityModelDayOfWeek, remaining: Int) : this("",userId,habitId, title,frequency, dayOfWeek, remaining)
     //    fun setRemaining(remaining: Int){ this.remaining = remaining }
     fun decrementRemaining(){ this.remaining-- }
 }
