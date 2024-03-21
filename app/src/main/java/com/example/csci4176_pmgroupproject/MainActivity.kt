c6ad6fa407c274ca9e83d47b8f84e924a061c2c2
Samiksha Sawhney <sm619491@dal.ca>
2024-03-21 20:24:22 -0300

    Modes Change

--- app/src/main/java/com/example/csci4176_pmgroupproject/MainActivity.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/MainActivity.kt
@@ -3,19 +3,30 @@ package com.example.csci4176_pmgroupproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, loginActivity::class.java))//this is only for testing rn
 
-//        val navbarFragment = NavigationBar()
-//
-//        // Add navbar fragment to the activity
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.navbarFrame, navbarFragment)
-//            .commit()
+
+        // startActivity(Intent(this, loginActivity::class.java))
+
+        val changeThemeSwitch = findViewById<Switch>(R.id.modeSwitch)
+
+        // Check the current mode and update the switch position accordingly
+        val isNightMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        changeThemeSwitch.isChecked = isNightMode
+
+        changeThemeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
