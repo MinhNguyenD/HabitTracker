27e287ead37548e81c703fced148fc79bfa346d8
Samuel Garcia <sam.garcia@dal.ca>
2024-03-25 19:15:02 -0300

    Modified fragment code Will obtain the values of each feature Set a listener for all toggles

--- app/src/main/java/com/example/csci4176_pmgroupproject/create_activity.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/create_activity.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ToggleButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,7 @@ class create_activity : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var daysOfWeek = arrayOf(false, false, false, false, false, false, false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +38,27 @@ class create_activity : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_activity, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activityTitleView = view.findViewById<EditText>(R.id.new_activity_title)
+        val activityNoteSection = view.findViewById<EditText>(R.id.new_activity_note)
+        /* Selected Days */
+        val DOWtoggles = arrayOf(view.findViewById<ToggleButton>(R.id.dow_sunday),
+            view.findViewById<ToggleButton>(R.id.dow_monday),
+            view.findViewById<ToggleButton>(R.id.dow_tuesday),
+            view.findViewById<ToggleButton>(R.id.dow_wednesday),
+            view.findViewById<ToggleButton>(R.id.dow_thursday),
+            view.findViewById<ToggleButton>(R.id.dow_friday),
+            view.findViewById<ToggleButton>(R.id.dow_saturday)
+        )
+        /* set listen for all */
+        for (i in 0..6){
+            DOWSwitchListener(DOWtoggles[i], i)
+        }
+
     }
 
     companion object {
@@ -55,5 +79,12 @@ class create_activity : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+    private fun DOWSwitchListener(toggle: ToggleButton, index:Int){
+       toggle.setOnCheckedChangeListener { button, checked ->
+           daysOfWeek[index] = checked
+       }
     }
 }
