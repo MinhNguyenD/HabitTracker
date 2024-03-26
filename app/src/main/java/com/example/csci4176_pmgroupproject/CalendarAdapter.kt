ee35697f7dd29f260a352380e6728212ca02b171
IaErG <igoncalves@carboncure.com>
2024-03-26 14:59:51 -0300

    Added logic for calculating mood and energy for the day

--- app/src/main/java/com/example/csci4176_pmgroupproject/CalendarAdapter.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/CalendarAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.csci4176_pmgroupproject
 
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.roundToInt
 
 class CalendarAdapter(
     private val daysOfMonth: ArrayList<String>,
@@ -25,7 +29,10 @@ class CalendarAdapter(
     }
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int)
-    { holder.dayOfMonth.text = daysOfMonth[position] }
+    {
+        holder.dayOfMonth.text = daysOfMonth[position]
+        updateCircleColour(holder, daysOfMonth[position])
+    }
 
     override fun getItemCount(): Int
     { return daysOfMonth.size }
@@ -33,6 +40,33 @@ class CalendarAdapter(
     interface OnItemListener
     { fun onItemClick(position: Int, dayText: String) }
 
+    private fun updateCircleColour(holder: CalendarViewHolder, day: String)
+    {
+        if (day == "")
+        { holder.circle.drawable.setTint(Color.TRANSPARENT) }
+
+        else
+        {
+            DatabaseAPI.getDailyActivitiesOnDate(day) { dayActivity ->
+                if (dayActivity.size > 0)
+                {
+                    var mood = 0.0
+                    var energy = 0.0
+                    for (activity in dayActivity)
+                    {
+                        mood += activity.mood.ordinal + 1 // Avoid 0 as a sum
+                        energy += activity.energy.ordinal + 1
+                    }
+
+                    val avgMood: Int = (mood / dayActivity.size - 1).roundToInt()
+                    val avgEnergy = (energy / dayActivity.size - 1).roundToInt()
+                }
+
+                else
+                { holder.circle.drawable.setTint(Color.GREEN) }
+            }
+        }
+    }
 }
 
 // View holder for the days of the month
@@ -41,6 +75,7 @@ class CalendarViewHolder(
     : RecyclerView.ViewHolder(itemView), View.OnClickListener
 {
     val dayOfMonth = itemView.findViewById(R.id.cellDayText) as TextView
+    val circle = itemView.findViewById(R.id.moodCircle) as ImageView
 
     init
     { itemView.setOnClickListener(this) }
