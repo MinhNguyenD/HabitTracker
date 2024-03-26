023e086169cb8d75533da48ae6715326bbd96e5e
noprogrammernolife <y2295019@edu.cc.uec.ac.jp>
2024-03-26 11:24:21 +0800

    reward

--- app/src/main/java/com/example/csci4176_pmgroupproject/User.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/User.kt
@@ -2,5 +2,5 @@ package com.example.csci4176_pmgroupproject
 
 /*TODO: create var of type Array<Habit>*/
 /*TODO: create badge feature*/
-/*TODO: reward */
+/*TODO: reward branch */
 data class User(val uid:String, var reward: String){ lateinit var username: String }
