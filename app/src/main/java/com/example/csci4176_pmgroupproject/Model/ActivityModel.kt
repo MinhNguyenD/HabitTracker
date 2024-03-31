a14f41056361ad6e5c1ee0b7a445dabff8eab4cd
Samuel Garcia <sam.garcia@dal.ca>
2024-03-31 02:57:14 -0300

    Modified Activity Model to be parcelable

--- app/src/main/java/com/example/csci4176_pmgroupproject/Model/ActivityModel.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/Model/ActivityModel.kt
@@ -1,8 +1,10 @@
 package com.example.csci4176_pmgroupproject.Model
 
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
 import com.example.csci4176_pmgroupproject.ActivityEnergy
-import com.example.csci4176_pmgroupproject.ActivityModel.ActivityModelDays
-import com.example.csci4176_pmgroupproject.ActivityModelDayOfWeek
 import com.example.csci4176_pmgroupproject.ActivityModelEnums
 import com.example.csci4176_pmgroupproject.ActivityModelFrequency
 import com.example.csci4176_pmgroupproject.ActivityMood
@@ -14,7 +16,7 @@ abstract class ActivityModel (var taskId : String, val userId : String, val habi
                               var isFinished : Boolean, val startDate : String,
                               var frequency : ActivityModelFrequency, var days: ArrayList<DayOfWeek>,
                               var streak: Int, var mood: ActivityMood,
-                              var energy: ActivityEnergy, var note: String){
+                              var energy: ActivityEnergy, var note: String) : Parcelable {
 
     constructor(habitId : String, title: String, type: ActivityModelEnums,
                 isFinished : Boolean, startDate : String,
@@ -39,4 +41,26 @@ abstract class ActivityModel (var taskId : String, val userId : String, val habi
     abstract fun complete()
 
     abstract fun reset()
+    override fun describeContents(): Int { return 0 }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeString(taskId)
+        p0.writeString(userId)
+        p0.writeString(habitId)
+        p0.writeString(title)
+        p0.writeInt(type.ordinal)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            p0.writeBoolean(isFinished)
+        }else {
+            p0.writeByte(if (isFinished) 1 else 0)
+        }
+        p0.writeString(startDate)
+        p0.writeInt(frequency.ordinal)
+        p0.writeList(days)
+        p0.writeInt(streak)
+        p0.writeInt(mood.ordinal)
+        p0.writeInt(energy.ordinal)
+        p0.writeString(note)
+    }
 }
--- app/src/main/java/com/example/csci4176_pmgroupproject/ModifyActivity.kt
+++ app/src/main/java/com/example/csci4176_pmgroupproject/ModifyActivity.kt
@@ -1,11 +1,48 @@
 package com.example.csci4176_pmgroupproject
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.example.csci4176_pmgroupproject.ActivityModel.ActivityModelEnums
+import com.example.csci4176_pmgroupproject.Model.ActivityModel
+import com.example.csci4176_pmgroupproject.Model.CheckedActivityModel
+import com.example.csci4176_pmgroupproject.Model.CountableActivityModel
+import com.example.csci4176_pmgroupproject.Model.TimedActivityModel
+
 class ModifyActivity : Fragment(){
+    private var activityModel: ActivityModel? = null
+
+    companion object {
+        private const val ARG_ACT = "activityModel"
+        private const val ARG_TYPE = "activityModelType"
+
+        fun newInstance(type: ActivityModelEnums, activityModel: ActivityModel?): ModifyActivity {
+            val fragment = ModifyActivity()
+            val args = Bundle().apply {
+                putParcelable(ARG_ACT, activityModel)
+            }
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            var type = it.getString(ARG_TYPE)
+            when (type){
+                ActivityModelEnums.CHECKED.toString() -> activityModel = it.getParcelable(ARG_ACT, CheckedActivityModel::class.java)
+                ActivityModelEnums.COUNTABLE.toString() -> activityModel = it.getParcelable(ARG_ACT, CountableActivityModel::class.java)
+                ActivityModelEnums.TIMED.toString() -> activityModel = it.getParcelable(ARG_ACT, TimedActivityModel::class.java)
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activityTitleView = view.findViewById<EditText>(R.id.new_activity_title)
